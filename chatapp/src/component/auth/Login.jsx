b0ae3f4cfdfa539af721d6cb027195ed4d49c34f
mertyusaatag <mertatag0@gmail.com>
2021-05-05 01:13:13 +0300

    Day 1

---
+++ chatapp/src/component/auth/Login.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {Form,Segment,Button,Grid,Message} from "semantic-ui-react"
+import {Link} from "react-router-dom"
+import styles from "./login.module.css";
+
+const Login = () => {
+    const handleSubmit = event =>{
+        event.preventDefault();
+    }
+
+
+
+    return (
+        <div>
+            
+           <Grid textAlign="center" 
+           verticalAlign="middle" 
+           className={styles.container}>
+               <Grid.Column style={{maxWidth:450}}>
+                   <h1 className={styles.formHeader}>Chat App</h1>
+
+                   <Form size="large" className={styles.form} onSubmit={handleSubmit}>
+
+                       <Segment>
+                           <Form.Input fluid icon="mail" iconPosition="left" name="email" placeholder="Email" type="email">   
+                           </Form.Input>,
+                           <Form.Input fluid icon="lock" iconPosition="left" name="password" placeholder="Password" type="password">   
+                           </Form.Input>
+                           <Form.Input fluid icon="lock" iconPosition="left" name="password" placeholder="Password" type="password">   
+                           </Form.Input>
+
+                           <Button color="grey" fluid size="large">Giriş Yap</Button>
+                       </Segment>
+                       <Message>
+                           Henüz üye değil misin ? <Link to="/signup">Hesap Oluştur</Link>
+                       </Message>
+                   </Form>
+
+               </Grid.Column>
+
+
+
+
+           </Grid>
+        </div>
+    )
+}
+
+export default Login
---
+++ chatapp/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import './index.css';
+import App from './App';
+import SignUp from "./component/auth/SignUp";
+import Login from './component/auth/Login';
+
+const Root = () => {
+  return(<Router>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route path="/signup" component={SignUp}/>
+      <Route path="/login" component={Login}/>
+    </Switch>
+  </Router>);
+  
+}
+
+
+ReactDOM.render(<Root />,document.getElementById('root'));
+
