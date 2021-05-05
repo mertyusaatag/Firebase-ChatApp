b9799d28c8d6edb3b47357143b78f1bcc8a92d52
mertyusaatag <mertatag0@gmail.com>
2021-05-05 23:07:13 +0300

    Day 2

--- chatapp/src/index.js
+++ chatapp/src/index.js
@@ -9,21 +9,44 @@ import {
 } from "react-router-dom";
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
 import SignUp from "./component/auth/SignUp";
 import firebase from "./firebase"
+import store from "./store/store"
 import Login from './component/auth/Login'
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from 'react-redux-firebase'
+
+const fbConfig={};
+
+const rrfConfig = {
+  userProfile: 'users'
+}
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch
+}
 
 const Root = () => {
-  return(<Router>
+  return(
     <Switch>
       <Route exact path="/" component={App}/>
       <Route path="/signup" component={SignUp}/>
       <Route path="/login" component={Login}/>
-    </Switch>
-  </Router>);
+    </Switch>);
   
 }
 
 
-ReactDOM.render(<Root />,document.getElementById('root'));
+ReactDOM.render( <Provider store={store}>
+  <ReactReduxFirebaseProvider {...rrfProps}>
+  <Router>
+    <Root/>
+    </Router>
+  </ReactReduxFirebaseProvider>
+</Provider>,document.getElementById('root'));
 
