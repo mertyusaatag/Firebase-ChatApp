8f6c5c28375192811373989147e614419fde464b
mertyusaatag <mertatag0@gmail.com>
2021-05-07 01:00:37 +0300

    Day 3

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -7,16 +7,16 @@ const App = () => {
   const currentChannel = useSelector((state)=> state.channels.currentChannel);
   return (
     <Grid columns="2" style={{ background: "#eee", height: "110vh" }}>
-    <Grid.Row>
-      <Grid.Column width={3}>
-        <SidePanel />
-      </Grid.Column>
+      <Grid.Row>
+        <Grid.Column width={3}>
+          <SidePanel />
+        </Grid.Column>
 
-      <Grid.Column style={{ background: "#fff" }} width={13}>
-      {currentChannel && <ChatPanel currentChannel={currentChannel}/>} 
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+        <Grid.Column style={{ background: "#fff" }} width={13}>
+          {currentChannel && <ChatPanel currentChannel={currentChannel} />}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   )
 }
 
--- chatapp/src/component/ChatPanel/ChatPanel.js
+++ chatapp/src/component/ChatPanel/ChatPanel.js
@@ -1,20 +1,64 @@
-import React from 'react'
-import {Segment ,Header,Icon,Comment,Form,Input,Button}
+import React,{useState} from 'react'
+import {Segment ,Header,Icon,Comment,Form,Input,Button, StepContent}
 from "semantic-ui-react";
 import {useFirebase} from "react-redux-firebase";
 import {useSelector} from "react-redux";
 const ChatPanel = ({currentChannel}) => {
+    const [searchTerm,setSearchTerm] = useState("");
+    const [content,setContent] = useState("");
+
     return (
         <>
         <Segment clearing>
-            <Header as ="h2" floated="left">
+            <Header as ="h3" floated="left">
                 <span>
                     <Icon name="hashtag"/>
                     {currentChannel.name}
                 </span>
-            </Header>
+            </Header>     
+            <Header as ="h3" floated="right"
+        >
+        <Input
+        size="mini"
+        icon="search"
+        name="searchTerm"
+        placeholder="Mesajlarda ara..."
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}>
+        </Input>
+        
+        </Header>
         </Segment>
 
+        {/*Message */}
+        <Segment style={{position:"fixed",top:55,bottom:70,width:"81%"}}>
+
+            <Comment.Group
+            style={{
+                height:"80vh",
+                overflowY:"auto",
+                maxWidth:"100%"
+            }}>
+            </Comment.Group>
+        </Segment>
+        {/* Send New Message */}
+
+        <Segment style={{
+                position:"fixed",
+                bottom:0,
+                width:"85%",
+                display:"flex"
+            }}>
+                <Button icon>
+                    <Icon name="add"/>
+                </Button>
+            <Form>
+                <Input fluid name="message" value={content}
+                onChange={(event) => StepContent(event.target.value) }
+                labelPosition="left" placeholder={`#${currentChannel.name} kanılına mesaj gönder`}/>
+            </Form>
+
+        </Segment>
 
         </>
 
